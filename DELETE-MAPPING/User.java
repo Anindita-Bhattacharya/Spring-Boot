c28d5a82d101c29634b6d40369a755013d0a163c
Anindita Bhattacharya <59307308+Anindita-Bhattacharya@users.noreply.github.com>
2025-06-06 11:31:15 +0530

    Create User.java

---
+++ DELETE-MAPPING/User.java
@@ -0,0 +1,40 @@
+package firstspring;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Users")
+public class User {
+    // Getters and Setters
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id; // Primary key
+    private String name; // User's name
+    private String email; // User's email
+    // Default constructor
+    public User() {}
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
